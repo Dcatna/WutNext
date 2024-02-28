11649e0ed2c860c409380575004f814e570ef53c
ddcat <ddcatena@gmail.com>
2024-02-27 23:31:21 -0500

    kinda fixed the wrong movie pause

--- src/Pages/IconScreens/RecommendedScreen.tsx
+++ src/Pages/IconScreens/RecommendedScreen.tsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useMemo, useState } from 'react'
 import Navbar from '../Navbar/Navbar'
 import { CurrentUserContext, TMDBClientContext } from '../../App'
 import { useInfiniteQuery } from '@tanstack/react-query'
-import { MovieListResult } from '../../data/types/MovieListResponse'
+import { MovieListResponse, MovieListResult } from '../../data/types/MovieListResponse'
 import Moviebox from '../../Components/Moviebox'
 
 
@@ -15,10 +15,12 @@ const RecommendedScreen = () => {
   let firstind = 0
   let secondind = 2 
   let realPage = 1
+
   const client = useContext(TMDBClientContext)
   const [recGenres, setRecGenres] = useState<number[]>([])
   const [recActors, setRecActors] = useState<string[]>([])
   const [passInGenres, setPassInGenres] = useState<number[]>(recGenres.slice(0, 1))
+  const [loading, setLoading] = useState<boolean>(true)
   const currentUserSession = useContext(CurrentUserContext) 
   const userToken = currentUserSession?.access_token
   useEffect(() => {
@@ -44,18 +46,31 @@ const RecommendedScreen = () => {
         console.error('There was a problem with the fetch operation:', error)
       }
     }
-
     fetchRecs();
   }, [])
-  
+
   console.log(passInGenres)
   const { data, fetchNextPage, isFetchingNextPage, hasNextPage } = useInfiniteQuery({
     queryKey: ['discoverMovies', recGenres.slice(0,2)],
-    queryFn: ({ pageParam = 1 }) => client.fetchRecommendedList(pageParam, "movie", passInGenres, recActors),
+    queryFn: ({ pageParam = 1 }) => recGenres.length === 0 
+    ? Promise.resolve<MovieListResponse>({
+        page: 1,
+        results: [],
+        total_pages: 0,
+        total_results: 0
+      }) : client.fetchRecommendedList(pageParam, "movie", passInGenres, recActors),
     getNextPageParam: (lastPage) => lastPage.page < lastPage.total_pages ? lastPage.page + 1 : undefined,
-    initialPageParam: 1, // Explicitly set initialPageParam
+    initialPageParam: 1, 
   });
-  
+  useEffect(() => {
+    if(data == null){
+      setLoading(true)
+    }
+    else{
+      setLoading(false)
+    }
+  }, [data])
+
 
   useEffect(() => {
       const handleScroll = () => {
@@ -84,7 +99,7 @@ const RecommendedScreen = () => {
 
   console.log(itemsRec)
 
-  
+  if(loading == true) return <div>Loading...</div>
   return (
     <div className="flex flex-col h-full w-full pe-12">
       <div>
