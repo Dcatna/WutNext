f85fb9ae8cc09a44625152343374e4a5e29fe769
ddcat <ddcatena@gmail.com>
2024-05-11 00:13:11 -0400

    can view and hide replies

--- src/Components/CommentBox.tsx
+++ src/Components/CommentBox.tsx
@@ -4,15 +4,25 @@ import { supabase } from '../lib/supabaseClient'
 import { number } from 'yup'
 import defaultimage from "./user_default.jpg"
 import { CommentWithReply } from './CommentPopup'
+import { Divide } from 'lucide-react'
 type Props = {}
 export interface Comment{
     comment : CommentWithReply
     singleComment : boolean
 }
+interface replyType{
+    id : number,
+    created_at : string,
+    user_id : string,
+    message : string,
+    cid : number
+}
 const CommentBox = ({comment, singleComment} : Comment) => {
     //const [userImage, setUserImage] = useState<string>()
     const date = new Date(comment.created_at)
     const [image, setImage] = useState<string>("")
+    const [showReplies, setShowReplies] = useState(false)
+    const [replies, setReplies] = useState<replyType[]>([])
     const getImageUrl = () => {
         
         if (comment.profile_image) {
@@ -27,10 +37,21 @@ const CommentBox = ({comment, singleComment} : Comment) => {
             setImage(defaultimage)
         }
     }
+    async function getReplies(){
+        const {data, error} = await supabase.from("reply").select("*").eq("cid", comment.id)
+        if(error) {
+            throw error
+        }else{
+            setReplies(data as replyType[])
+        }
+    }
     useEffect(() => {
         getImageUrl()
-    },[])
-    console.log(image)
+        getReplies()
+    },[showReplies])
+    const toggleReplies = () => {
+        setShowReplies(!showReplies)
+    }
   return (
     <div className='flex'>
         <img className="rounded-full h-20 w-20 border-2 border-gray-300" src={image} alt="" />
@@ -59,7 +80,17 @@ const CommentBox = ({comment, singleComment} : Comment) => {
                 <p className='break-words'>{comment.message}</p>
             </div> : <div className='text-black break-words overflow-hidden w-[400px] flex flex-col'>
                 <p className='break-words'>{comment.message}</p>
-                {comment.replies == 0 ? <div></div> : <p className='break-words'>--Replies {comment.replies}</p>}
+                {comment.replies > 0 && !showReplies && <p onClick={toggleReplies} className='break-words ml-2 cursor-pointer'>View {comment.replies} more reply</p>}
+                {showReplies && (
+                    <div className='ml-2'>
+                        {replies.map((reply : replyType) => (
+                            <div>
+                                {reply.message}
+                            </div>
+                        ))}
+                        <p className = "cursor-pointer" onClick={toggleReplies}>Hide Replies</p>
+                    </div>
+                )}
                 
             </div>}
         </div>
